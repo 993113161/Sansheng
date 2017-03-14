6ef16e92ae86502bea118ca19c8d5c7ac7c9c1cc
Victor <993113161@qq.com>
2017-03-14 16:36:28 +0800

    add image of listItem
    在从中假如人物，而且使其不能够竖屏

--- app/src/main/java/com/example/imac/sansheng/DetailActivity.java
+++ app/src/main/java/com/example/imac/sansheng/DetailActivity.java
@@ -3,11 +3,13 @@ package com.example.imac.sansheng;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView tv;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +20,31 @@ public class DetailActivity extends AppCompatActivity {
         int position = intent.getIntExtra("position", 0);
 
         tv = (TextView) findViewById(R.id.qwe);
+        iv = (ImageView) findViewById(R.id.iv);
 //        tv.setText(position + "");
         switch (position){
-            case 0: tv.setText(getResources().getString(R.string.baiqian));break;
-            case 1: tv.setText(getResources().getString(R.string.yehua));break;
-            case 2: tv.setText(getResources().getString(R.string.zheyan));break;
-            case 3: tv.setText(getResources().getString(R.string.fengjiu));break;
-            case 4: tv.setText(getResources().getString(R.string.qingcang));break;
-            case 5: tv.setText(getResources().getString(R.string.donghua));break;
+            case 0:
+                break;
+            case 1:
+                tv.setText(getResources().getString(R.string.yehua));
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.yehua));
+                break;
+            case 2:
+                tv.setText(getResources().getString(R.string.zheyan));
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.zheyan));
+                break;
+            case 3:
+                tv.setText(getResources().getString(R.string.fengjiu));
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.fengjiu));
+                break;
+            case 4:
+                tv.setText(getResources().getString(R.string.qingcang));
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.qingcang));
+                break;
+            case 5:
+                tv.setText(getResources().getString(R.string.donghua));
+                iv.setImageDrawable(getResources().getDrawable(R.drawable.donghua));
+                break;
         }
     }
 }
--- app/src/main/java/com/example/imac/sansheng/MainActivity.java
+++ app/src/main/java/com/example/imac/sansheng/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
         setContentView(R.layout.activity_main);
 
 
